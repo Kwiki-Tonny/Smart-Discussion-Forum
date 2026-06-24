5c8b745822d0314115e80bb1b4f0c6dd51839ac6
Kakande Alice Dorothy <dorothyalice247@gmail.com>
2026-06-24 10:46:07 +0300

    DesktopClient

---
+++ student-client-java/DesktopClient/javaclient/src/main/java/com/discussionforum/App.java
@@ -0,0 +1,13 @@
+package com.discussionforum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ student-client-java/DesktopClient/javaclient/src/main/java/com/discussionforum/DatabaseHandler.java
@@ -0,0 +1,5 @@
+package com.discussionforum;
+
+public class DatabaseHandler {
+    
+}
