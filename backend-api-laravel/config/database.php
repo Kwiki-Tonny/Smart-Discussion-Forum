1a9f80e05135161b039e7b8cef60b161fe2d4354
Ssenkuba Tonny Kwikiriza <ssenkuba.tonnykwikiriza@students.mak.ac.ug>
2026-07-22 01:55:01 +0300

    modify path

--- backend-api-laravel/config/database.php
+++ backend-api-laravel/config/database.php
@@ -60,7 +60,7 @@ return [
     'strict' => true,
     'engine' => null,
 'options' => array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => database_path('certs/aiven-ca.pem'),
+                PDO::MYSQL_ATTR_SSL_CA => database_path('certs/ca.pem'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
             ]),
 ],
