27db6ef50113b75e60056fdd1d16719ce71c79e9
Ssenkuba Tonny Kwikiriza <ssenkuba.tonnykwikiriza@students.mak.ac.ug>
2026-07-22 01:26:47 +0300

    Add SSL options for MySQL connection

--- backend-api-laravel/config/database.php
+++ backend-api-laravel/config/database.php
@@ -45,26 +45,28 @@ return [
         ],
 
 'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => array_merge(
-                extension_loaded('pdo_mysql') ? [
-                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                ] : [],
-            ),
-        ],
+    'driver' => 'mysql',
+    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'strict' => true,
+    'engine' => null,
+    'options' => [
+        // SSL options if needed
+        PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA'),
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY_SERVER_CERT', false),
+        PDO::MYSQL_ATTR_SSL_KEY => env('DB_SSL_KEY'),
+        PDO::MYSQL_ATTR_SSL_CERT => env('DB_SSL_CERT'),
+    ],
+],
 
         'mariadb' => [
             'driver' => 'mariadb',
